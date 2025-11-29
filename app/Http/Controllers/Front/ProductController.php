0a4bd8734056cfc9756067baac4a587bf799f8e0
Henry <a54321580@yahoo.com.tw>
2025-11-29 19:01:42 +0800

    完成product menu filter (sort待完成)

--- app/Http/Controllers/Front/ProductController.php
+++ app/Http/Controllers/Front/ProductController.php
@@ -11,19 +11,69 @@ use Inertia\Inertia;
 
 class ProductController extends Controller
 {
-    public function index()
+    // public function index(Request $request, ?string $categoryName = null)
+    // {
+    //     $categories_tab = Category::select(['id', 'name'])->orderBy('sort_order')->get();
+    //     $query = Product::query()->select(['id', 'subcategory_id', 'slug', 'name', 'price', 'description'])
+    //         ->with('primaryImage')
+    //         ->with('cheapestOption');
+    //     // ->get();
+
+    //     if ($categoryName) {
+    //         $category = Category::where('name', $categoryName)->first();
+
+    //         if ($category) {
+    //             $query->whereHas('subcategory', function ($q) use ($category) {
+    //                 $q->where('category_id', $category->id);
+    //             });
+    //         }
+    //     }
+
+    //     // if ($subcategoryId = $request->get('subcategory_id')) {
+    //     //     $query->where('subcategory_id', $subcategoryId);
+    //     // }
+
+    //     $products = $query->get();
+
+    //     return Inertia::render('Front/Products/Index', [
+    //         'categories_tab' => $categories_tab,
+    //         'products' => $products
+    //     ]);
+    // }
+
+    public function index(?Category $category, Request $request)
     {
-        $categories_tab = Category::select(['id', 'name'])->orderBy('sort_order')->get();
-        $products = Product::query()->select(['id', 'subcategory_id', 'slug', 'name', 'price', 'description'])
-        ->with('primaryImage')
-        ->with('cheapestOption')
-        ->get();
-        
+        // dd($category);
+        $categories_tab = Category::select(['id', 'name'])
+            ->orderBy('sort_order')
+            ->get();
+
+        $query = Product::query()
+            ->select(['id', 'subcategory_id', 'slug', 'name', 'price', 'description'])
+            ->with('primaryImage')
+            ->with('cheapestOption');
+
+        if ($category && $category->exists) {
+            $query->whereHas('subcategory', function ($q) use ($category) {
+                $q->where('category_id', $category->id);
+            });
+        }
+
+        // dd(
+        //     $query->toSql(),        // 帶 ? 的 SQL 字串
+        //     $query->getBindings()   // 對應的參數
+        // );
+
+        $products = $query->get();
+
+
         return Inertia::render('Front/Products/Index', [
             'categories_tab' => $categories_tab,
-            'products' => $products
+            'products'       => $products,
+            'activeCategory'  => $category?->name,
         ]);
     }
+
 
     public function home()
     {
--- routes/front.php
+++ routes/front.php
@@ -6,7 +6,9 @@ use Illuminate\Support\Facades\Route;
 // Route::resource('product', ProductController::class);
 
 Route::get('/', [ProductController::class, 'home'])->name('front.home.index');
-Route::get('/products', [ProductController::class, 'index'])
+// Route::get('/products/{categoryName?}', [ProductController::class, 'index'])
+//     ->name('product.index');
+Route::get('/products/{category?}', [ProductController::class, 'index'])
     ->name('product.index');
 // Route::get('/products', [ProductController::class, 'index'])->name('front.products.index');
 // Route::get('/products/{product:slug}', [ProductController::class, 'show'])->name('front.products.show');
